b23fddce82b47d102c665626ec3eb66b7788d01c
D. Williams <d.williams@posteo.net>
2020-02-20 20:20:30 +0100

    Adding remainder forgotten by last commit

---
+++ org-menu.el
@@ -0,0 +1,36 @@
+;;; org-menu.el --- Hide verbose org syntax behind interactive menus.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  D. Williams, Rasmus Pank
+
+;; Author: D. Williams <d.williams@posteo.net>
+;; Maintainer: D. Williams <d.williams@posteo.net>
+;; Keywords: faces, outlines
+;; Version: 0
+;; Homepage: https://github.com/integral-dw/org-menu-mode
+;; Package-Requires: ((org "9.1.9") (emacs "26.2"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'org-menu)
+;;; org-menu.el ends here
