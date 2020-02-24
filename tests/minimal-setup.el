aa5fd9622ee6a74a63c1262fd6531edeabe48dcf
D. Williams <d.williams@posteo.net>
2020-02-25 00:30:38 +0100

    Updating minimal setup to eliminate annoying backup files.

--- tests/minimal-setup.el
+++ tests/minimal-setup.el
@@ -1,6 +1,7 @@
 ;;; A small init file setting you up for tests.
 (package-initialize)
-
+(setq backup-inhibited t)
+(setq auto-save-default nil)
 (add-hook 'org-mode-hook
           (lambda ()
             (org-menu-mode 1)))
