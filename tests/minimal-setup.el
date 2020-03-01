151d3e867abda05d4a52d1af890938625ae6657d
D. Williams <d.williams@posteo.net>
2020-03-01 17:27:11 +0100

    removed spurious package-initialize

--- tests/minimal-setup.el
+++ tests/minimal-setup.el
@@ -1,8 +1,9 @@
 ;;; A small init file setting you up for tests.
-(package-initialize)
 (require 'org-menu)
 (setq backup-inhibited t)
 (setq auto-save-default nil)
 (add-hook 'org-mode-hook
           (lambda ()
             (org-menu-mode 1)))
+;; (setq debug-on-error t)
+;; (setq debug-on-signal t)
